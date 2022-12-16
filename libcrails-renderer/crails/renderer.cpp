fba1adcbc96d72aaf4adc6195c429b93bbd651ec
plaristote <michael@unetresgrossebite.com>
2022-12-16 01:09:47 +0100

    refactore Renderers to use the SINGLETON macro instead of static variables

--- libcrails-renderer/crails/renderer.cpp
+++ libcrails-renderer/crails/renderer.cpp
@@ -4,15 +4,6 @@
 using namespace Crails;
 using namespace std;
 
-list<Renderer*> Crails::renderers;
-
-void Renderer::finalize()
-{
-  for (auto it = renderers.begin() ; it != renderers.end() ; ++it)
-    delete *it;
-  renderers.clear();
-}
-
 static string get_format(const string& format, const string& default_format)
 {
   return format.length() == 0 ? default_format : format;
@@ -23,18 +14,25 @@ void Renderer::render(const std::string& view, const string& accept, RenderTarge
   const Renderer* renderer = pick_renderer(view, accept);
 
   if (renderer == nullptr)
-    throw MissingTemplate(view, get_format(accept, default_format));
+  {
+    const Renderers* renderers = Renderers::singleton::get();
+    throw MissingTemplate(view, get_format(accept, renderers->get_default_format()));
+  }
   renderer->render_template(view, target, vars);
 }
 
-Renderer* Renderer::pick_renderer(const string& view, const string& accept)
+const Renderer* Renderer::pick_renderer(const string& view, const string& accept)
 {
-  string format = get_format(accept, default_format);
+  const Renderers* renderers = Renderers::singleton::get();
+  string format;
 
-  for (auto it = renderers.begin() ; it != renderers.end() ; ++it)
+  if (!renderers)
+    throw boost_ext::runtime_error("Crails::Renderers singleton hasn't been initialized.");
+  format = get_format(accept, renderers->get_default_format());
+  for (auto it = renderers->begin() ; it != renderers->end() ; ++it)
   {
     if ((*it)->can_render(format, view))
-      return *it;
+      return (*it).get();
   }
   return nullptr;
 }
--- libcrails-renderer/crails/renderer.hpp
+++ libcrails-renderer/crails/renderer.hpp
@@ -3,6 +3,8 @@
 
 # include <list>
 # include <map>
+# include <memory>
+# include <crails/utils/singleton.hpp>
 # include <crails/utils/backtrace.hpp>
 # include <crails/datatree.hpp>
 # include "shared_vars.hpp"
@@ -28,31 +30,44 @@ namespace Crails
     std::string name, message;
   };
 
+  class Renderer;
+
+  class Renderers
+  {
+    SINGLETON(Renderers)
+    typedef std::list<std::unique_ptr<const Renderer>> RendererList;
+  protected:
+    Renderers() {}
+    virtual ~Renderers() {}
+  public:
+
+    RendererList::const_iterator begin() const { return renderers.begin(); }
+    RendererList::const_iterator end() const { return renderers.end(); }
+    const std::string& get_default_format() const { return default_format; }
+
+  protected:
+    std::string  default_format = "*";
+    RendererList renderers;
+  };
+
   class Renderer
   {
   protected:
     typedef void (*Generator)(const Renderer&, RenderTarget&, SharedVars&);
     typedef std::map<std::string, Generator> Templates;
-
   public:
-    static void initialize();
-    static void finalize();
-
     virtual ~Renderer() {}
     virtual bool can_render(const std::string& accept_header, const std::string& view) const = 0;
     virtual void render_template(const std::string& view, RenderTarget&, SharedVars& vars) const = 0;
+    const Templates& get_templates() const { return templates; }
 
-    static Renderer*   pick_renderer(const std::string& view, const std::string& accept);
-    static bool        has_renderer(const std::string& view, const std::string& accept);
-    static void        render(const std::string& view, const std::string& accept, RenderTarget&, SharedVars& vars);
-    const Templates&   get_templates() const { return templates; }
+    static const Renderer* pick_renderer(const std::string& view, const std::string& accept);
+    static bool            has_renderer(const std::string& view, const std::string& accept);
+    static void            render(const std::string& view, const std::string& accept, RenderTarget&, SharedVars& vars);
 
   protected:
-    Templates                templates;
-    static const std::string default_format;
+    Templates templates;
   };
-
-  extern std::list<Renderer*> renderers;
 }
 
 #endif
